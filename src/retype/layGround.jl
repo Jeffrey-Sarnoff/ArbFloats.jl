23d1e0c6531f6c416e367367342ea004a8a8ab1b
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-27 13:46:20 -0400

    Update layGround.jl

--- src/retype/layGround.jl
+++ src/retype/layGround.jl
@@ -39,14 +39,14 @@ const FastArbPrecison = ArbPrecision( fld(480, (12-sizeof(Int))) )
 
 
 type MagStruct                           #  mag_struct (arb/master/mag.h)
-  rad_exp ::Int  #                       #  exponent    of 'radius' magnitude
-  rad_man ::UInt # 30? significand bits  #  significand of 'radius' magnitude
+  rad_xpn ::Int  #                       #  exponent    of 'radius' magnitude
+  rad_sgf ::UInt # 30? significand bits  #  significand of 'radius' magnitude
 end
 
-MagStruct() = MagStruct(zero(Int), zero(UInt))
+MagStruct() = MagStruct(sint0, sint0)
 
-type SignificandStruc     t              # mantissa_struct (arb/master/arf.h)
-  d1  ::Int      # mantissa_struct   imm mantissa value high or mantissa alloc size
+type SignificandStruct                   # mantissa_struct (arb/master/arf.h)
+  d1  ::Int      #                   imm mantissa value high or mantissa alloc size
   d2  ::Int      #                   imm mantissa value low     ptr2 mantissa value
 end
 
@@ -54,44 +54,75 @@ SignificandStruct() = SignificandStruct(sint0, sint0)
 SignificandStruct(d1::Int) = SignificandStruct(d1, sint0)
 
 type ArfStruct                           #  arf_struct (arb/master/arf.h) 
-  exp ::Int      # fmpz?
-  size::UInt     # mp_size_t
-  d1  ::Int      # mantissa_struct
+  xpn ::Int      # fmpz?
+  mpsz::UInt     # mp_size_t
+  d1  ::Int      # SignificandStruct     # mantissa_struct
   d2  ::Int      #
 end
 
 ArfStruct() = ArfStruct(zero(Int),zero(UInt),zero(Int),zero(Int))
 
+convert(::Type{SignificandStruct}, x::ArfStruct) = SignificandStruct(x.d1, x.d2)
 
-type ArbStruct                           #  arf_struct (arb/master/arf.h)
-  mid_exp ::Int  # fmpz                  #
-  mid_size::UInt # mp_size_t             #
-  mid_d1  ::Int  # mantissa_struct       #   
+convert(::Type{ArfStruct}, x::SignificandStruct, xp::Int, mpsz::UInt) = 
+    ArfStruct(xp, mpsz, x.d1, x.d2)
+convert(::Type{ArfStruct}, xp::Int, mpsz::UInt, x::SignificandStruct) = 
+    convert(ArfStruct, x, xp, mpsz)
+convert(::Type{ArfStruct}, mpsz::UInt, xp::Int, x::SignificandStruct) = 
+    convert(ArfStruct, x, xp, mpsz)
+
+
+type ArbStruct                           #  arb_struct (arb/master/arb.h)
+  mid_xpn ::Int  # fmpz                  #    arf_struct
+  mid_mpsz::UInt # mp_size_t             #
+  mid_d1  ::Int  # SignificandStruct     #       mantissa_strct
   mid_d2  ::Int                          #
-  rad_exp ::Int  # fmpz?                 #  mag_struct (arb/master/mag.h)
-  rad_man ::UInt                         #  mag_struct
+  rad_xpn ::Int  # fmpz?                 #       mag_struct
+  rad_sgf ::UInt                         #   
 end
 
 ArbStruct() = ArbStruct(sint0,uint0,sint0,sint0,sint0,uint0)
 
+convert(::Type{SignificandStruct}, x::ArbStruct) = SignificandStruct(x.d1, x.d2)
+
+convert(::Type{ArbStruct}, x::SignificandStruct, mid_xpn::Int, mid_mpsz::UInt) =
+    ArbStruct(mid_xpn, mid_mpsz, x.d1, x.d2, sint0, uint0)
+convert(::Type{ArbStruct}, mid_xpn::Int, mid_mpsz::UInt, x::SignificandStruct) =
+    convert(ArbStruct, x, mid_xpn, mid_mpsz)
+convert(::Type{ArbStruct}, mid_mpsz::UInt, mid_xpn::Int, x::SignificandStruct) =
+    convert(ArbStruct, x, mid_xpn, mid_mpsz)
+    
+
+convert(::Type{ArfStruct}, x::ArbStruct) = ArfStruct(x.mid_xpn, x.mid_mpsa, x.mid_d1, x.mid_d2)
+
+convert(::Type{ArbStruct}, x::ArfStruct, rad_xpn::Int, rad_sgf::UInt) =
+    ArbStruct(mid_xpn, mid_mpsz, x.d1, x.d2, sint0, uint0)
+convert(::Type{ArbStruct}, rad_xpn::Int, rad_sgf::UInt, x::ArfStruct) =
+    convert(ArbStruct, x, rad_xpn, rad_sgf)
+convert(::Type{ArbStruct}, rad_sgf::UInt, rad_xpn::Int, x::ArfStruct) =
+    convert(ArbStruct, x, rad_xpn, rad_sgf)
+
+
 
 type ArbValue # <: FieldElem
-  mid_exp  ::Int # fmpz
-  mid_size::UInt # mp_size_t
+  mid_xpn  ::Int # fmpz
+  mid_mpsz ::UInt # mp_size_t
   mid_d1   ::Int # mantissa_struct
   mid_d2   ::Int
-  rad_exp  ::Int # fmpz?
-  rad_man  ::UInt
+  rad_xpn  ::Int # fmpz?
+  rad_sgf  ::UInt
   parent   ::ArbPrecision
 end
 
 ArbValue() = ArbValue(sint0,uint0,sint0,sint0,sint0,uint0,FastArbPrecision)
 
-function convert(::Type{ArbStruct}, x::ArbValue)
+convert(::Type{ArbStruct}, x::ArbValue) =
     ArbStruct(x.mid_exp, x.mid_size, x.mid_d1, x.mid_d2, x.rad_exp, x.rad_man)
-end
 
+convert(::Type{ArbValue}, x::ArbStruct) =
+    ArbValue( x.mid_xpn, x.mid_mpsz, x.mid_d1, x.mid_d2, x.rad_xpn, x.rad_sgf, FastArbPrecision)
+    
 function convert(::Type{ArbValue}, x::ArbStruct, n::Int)
     arbprec = getkey(ArbPrecisions, n, (ArbPrecisions[n] = ArbPrecision(n))  )
-    ArbValue( x.mid_exp, x.mid_size, x.mid_d1, x.mid_d2, x.rad_exp, x.rad_man, arbprec )
+    ArbValue( x.mid_xpn, x.mid_mpsz, x.mid_d1, x.mid_d2, x.rad_xpn, x.rad_sgf, arbprec )
 end
