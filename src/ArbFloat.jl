5e398d6893b3fa784fa5741e304eb33cb4b18380
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-26 23:22:17 -0400

    Create ArbFloat.jl

---
+++ src/ArbFloat.jl
@@ -0,0 +1,16 @@
+module ArbFloat
+
+import Base: hash, convert, promote_rule, isa,
+    string, show, showcompact, showall, parse,
+    zero, one, ldexp, frexp, eps,
+    isequal, isless, (==),(!=),(<),(<=),(>=),(>)
+    min, max, minmax,
+    isnan, isinf, isfinite, issubnormal,
+    signbit, sign, flipsign, copysign, abs,
+    (+),(-),(*),(/),(\),(%),(^),sqrt,
+    trunc, round, ceil, floor,
+    fld, cld, div, mod, rem, divrem, fldmod,
+    
+    
+
+end # ArbFloat
