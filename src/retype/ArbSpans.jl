0fa29b02bc0f23661485f72d89058497e2b77d84
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-27 01:44:56 -0400

    Update ArbSpans.jl

--- src/retype/ArbSpans.jl
+++ src/retype/ArbSpans.jl
@@ -1,1 +1,13 @@
-
+"""
+"""
+type ArbSpan <: Real
+    midpoint::Real
+    halfspan::Real
+    
+    arbInside::
+    
+    ArbSpan{MP<:Float64, HS<:Float32}(midpoint::MP, halfspan::HS)
+        
+    end
+    
+end
