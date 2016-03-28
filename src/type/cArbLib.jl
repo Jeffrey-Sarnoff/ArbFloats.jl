5c756414ed465104b1c66f3f7e5ded3b8af098c1
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-28 18:35:23 -0400

    Update cArbLib.jl

--- src/type/cArbLib.jl
+++ src/type/cArbLib.jl
@@ -1,25 +1,33 @@
 
-type ArbValue{Prec} <: Real
-   significandExpo   ::Int
-   significandSize   ::UInt
-   significand1st    ::Int64
-   significand2nd    ::Int64
+type ArbValue <: Real
 
-   plusorminus2Expon ::Int
-   plusorminusSignif ::UInt
+   significandExpo ::Int
+   significandSize ::UInt
+   significand1st  ::Int
+   significand2nd  ::Int
+
+   plusminus2Expon ::Int
+   plusminusSignif ::UInt
    
-   ArbValue(significand:SignificandStruct, plusorminus::PlusOrMinusStruct, precision::Int) =
-       new{precision}( significand.significandExpo, significand.signifiandSize,
-                       significand.significand1st , significand.significand2nd,
-                       plusorminus.plusorminus2Expon, plusorminus.plusorminusSignif )
+   parentprecision ::Int
+   
+   ArbValue(significandExpo::Int, signifiandSize::UInt,
+            significand1st::Int , significand2nd::Int,
+            plusminus2Expon::Int, plusminusSignif::UInt, parentprecision::Imt ) =
+       new( significand.significandExpo, significand.signifiandSize,
+            significand.significand1st , significand.significand2nd,
+            plusminus.plusminus2Expon, plusminus.plusminusSignif, parentprecision )
 end
 
-ArbValue(significand::SignificandStruc, plusorminus::PlusOrMinusStruct, precision::Int) =
-    ArbValue{precision}(significand.significandExpo, significand.signifiandSize,
-                       significand.significand1st , significand.significand2nd,
-                       plusorminus.plusorminus2Expon, plusorminus.plusorminusSignif )
+ArbValue(significand:SignificandStruct, plusorminus::PlusOrMinusStruct, parentprecision::Int) =
+   ArbValue( significand.significandExpo, significand.signifiandSize,
+             significand.significand1st , significand.significand2nd,
+             plusminus.plusminus2Expon, plusminus.plusminusSignif, parentprecision )
 
-ArbValue(significand::SignificandStruc, plusorminus::PlusOrMinusStruct, bitsOfPrecision::Integer) =
-   ArbValue( significand, plusorminus, max(10, min(bitsOfPrecison, 30_000)) )
+ArbValue(significand::SignificandStruc, plusorminus::PlusOrMinusStruct) =
+   ArbValue( significand.significandExpo, significand.signifiandSize,
+             significand.significand1st , significand.significand2nd,
+             plusorminus.plusorminus2Expon, plusorminus.plusorminusSignif,
+             parentprecision() )
 
 
