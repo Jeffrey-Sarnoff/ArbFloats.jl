17add4241aa09ec8fd7f6b75a5c39e480d09f3f9
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-28 18:38:19 -0400

    Update cArbLib.jl

--- src/type/cArbLib.jl
+++ src/type/cArbLib.jl
@@ -13,11 +13,13 @@ type ArbValue <: Real
    
    ArbValue(significandExpo::Int, signifiandSize::UInt,
             significand1st::Int , significand2nd::Int,
-            plusminus2Expon::Int, plusminusSignif::UInt, parentprecision::Imt ) =
+            plusminus2Expon::Int, plusminusSignif::UInt, parentprecision::Int ) =
        new( significand.significandExpo, significand.signifiandSize,
             significand.significand1st , significand.significand2nd,
             plusminus.plusminus2Expon, plusminus.plusminusSignif, parentprecision )
 end
+
+parentprecision() = precision(ArbValue) # ::Int
 
 ArbValue(significand:SignificandStruct, plusorminus::PlusOrMinusStruct, parentprecision::Int) =
    ArbValue( significand.significandExpo, significand.signifiandSize,
