aff22d463c4aa314c5cfdb0e06e447a0dfd10378
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-29 07:52:23 -0400

    Update MagLib.jl

--- src/api/MagLib.jl
+++ src/api/MagLib.jl
@@ -1,1 +1,8 @@
+#=
 
+The mag_t type is an unsigned floating-point type with a fixed-precision mantissa (30 bits) and an arbitrary-precision exponent (represented as an fmpz_t), suited for representing and rigorously manipulating magnitude bounds efficiently.
+
+Operations always produce a strict upper or lower bound, but for performance reasons, no attempt is made to compute the best possible bound (in general, a result may a few ulps larger/smaller than the optimal value). The special values zero and positive infinity  are supported (but not NaN). Applications requiring more flexibility (such as correct rounding, or higher precision) should use the arf_t type instead.
+
+-- (mag.h)[http://fredrikj.net/arb/mag.html#c.mag_struct]
+=#
