628f64a6a99b1b4a49d93eff82442ffd4e83142b
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-28 20:41:24 -0400

    Update cArbLib.jl

--- src/type/cArbLib.jl
+++ src/type/cArbLib.jl
@@ -1,35 +1,39 @@
 
+# ArbValue := (midpoint, halfwdith)
+# lobound.halfwidth.midpoint.halfwidth.hibound
+#    fp      span      fp        spn     fp
+
 type ArbValue <: Real
-
-   significandExpo ::Int
+   # midpoint of interval value
+   significandPow2 ::Int
    significandSize ::UInt
-   significand1st  ::Int
-   significand2nd  ::Int
-
-   plusminus2Expon ::Int
-   plusminusSignif ::UInt
+   significandHead ::Int
+   significandTail ::Int
+   # halfwidth of interval value, symmetric about the midpoint 
+   halfwidthPow2   ::Int
+   halfwidthSignif ::UInt
    
    parentprecision ::Int
    
-   ArbValue(significandExpo::Int, signifiandSize::UInt,
-            significand1st::Int , significand2nd::Int,
-            plusminus2Expon::Int, plusminusSignif::UInt, parentprecision::Int ) =
-       new( significand.significandExpo, significand.signifiandSize,
-            significand.significand1st , significand.significand2nd,
-            plusminus.plusminus2Expon, plusminus.plusminusSignif, parentprecision )
+   ArbValue(significandPow2::Int, signifiandSize::UInt,
+            significandHead::Int , significandTail::Int,
+            halfwidthPow2::Int, halfwidthSignif::UInt, parentprecision::Int ) =
+       new( significandPow2, significandSize,
+            significandHead, significandTail,
+            halfwidthPow2 halfwidthSignif, parentprecision )
 end
 
 parentprecision() = precision(ArbValue) # ::Int
 
 ArbValue(significand:SignificandStruct, plusorminus::PlusOrMinusStruct, parentprecision::Int) =
-   ArbValue( significand.significandExpo, significand.signifiandSize,
-             significand.significand1st , significand.significand2nd,
-             plusminus.plusminus2Expon, plusminus.plusminusSignif, parentprecision )
+   ArbValue( significand.significandPow2, significand.signifiandSize,
+             significand.significandHead, significand.significandTail,
+             halfwidthPow2, halfwidthSignif, parentprecision )
 
 ArbValue(significand::SignificandStruc, plusorminus::PlusOrMinusStruct) =
-   ArbValue( significand.significandExpo, significand.signifiandSize,
-             significand.significand1st , significand.significand2nd,
-             plusorminus.plusorminus2Expon, plusorminus.plusorminusSignif,
+   ArbValue( significand.significandPow2, significand.signifiandSize,
+             significand.significandHead, significand.significandTail,
+             halfwidthPow2, halfwidthSignif, parentprecision,
              parentprecision() )
 
 
