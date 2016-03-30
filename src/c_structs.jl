603d08fb56bfaf0e9b427affa630f4a79d1ad637
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-30 09:18:13 -0400

    Update c_structs.jl

--- src/c_structs.jl
+++ src/c_structs.jl
@@ -58,7 +58,6 @@
    fmpr_struct;
 
    
-   
    typedef   signed long        fmpz;
    typedef unsigned long        mp_limb_t;
    typedef   signed long        mp_limb_signed_t;
@@ -88,34 +87,32 @@ immutable fmpr_struct
     fmpz                        man # mantissa
     fmpz                        exp # exponent
 end
-
 typealias fmpr_struct_ptr       Ref{fmpr_struct}
 
 immutable mag_struct
     fmpz                        exp # exponent
     mp_limb_t                   man # mantissa
 end
-
 typealias mag_struct_ptr        Ref{mag_struct}
-
 
 immutable mantissa_ptr_struct
     mp_size_t                   alloc
     mp_ptr                      d
 end
+typealias mantissa_ptr_struct_ptr     Ref{mantissa_ptr_struct}
 
 immutable mantissa_noptr_struct
     mp_limb_t                   d[ARF_NOPTR_LIMBS]
 end
+typealias mantissa_noptr_struct_ptr   Ref{mantissa_noptr_struct}
 
 type mantissa_struct                         # in C, a Union type
 #   mantissa_noprt_struct   noptr            # drop any smaller members
     mantissa_ptr_struct     ptr              # specify one member, the largest
 end
-
-typealias mantissa_ptr_struct_ptr     Ref{mantissa_ptr_struct}
-typealias mantissa_noptr_struct_ptr   Ref{mantissa_noptr_struct}
 typealias mantissa_struct_ptr         Ref{mantissa_struct}
+
+
 
 
 
