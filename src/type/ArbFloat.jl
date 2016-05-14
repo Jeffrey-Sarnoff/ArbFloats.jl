fce56ff9681033b1431f9c7e25e48bda8779330b
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-14 04:10:51 -0400

    Update ArbFloat.jl

--- src/type/ArbFloat.jl
+++ src/type/ArbFloat.jl
@@ -16,36 +16,36 @@ precision{Precision}(x::ArbFloat{Precision}) = Precision
 
 
 function clearArbFloat(x::ArbFloat)
-     ccall((:arb_clear, :libarb), Void, (Ptr(ArbFloat),), &x)
+     ccall((:arb_clear, libarb), Void, (Ptr(ArbFloat),), &x)
 end
 
 function ArbFloat()
     z = ArbFloat{precision(ArbFloat)}(0,0,0,0,0,0)
-    ccall((:arb_init, :libarb), Void, (Ptr{ArbFloat},), &z)
+    ccall((:arb_init, libarb), Void, (Ptr{ArbFloat},), &z)
     finalizer(z, clearArbFloat)
     z
 end
 
 function ArbFloat(x::UInt)
     z = ArbFloat{precision(ArbFloat)}(0,0,0,0,0,0)
-    ccall((:arb_init, :libarb), Void, (Ptr{ArbFloat},), &z)
-    ccall((:arb_set_ui, :libarb), Void, (Ptr{ArbFloat}, UInt), &z, x)
+    ccall((:arb_init, libarb), Void, (Ptr{ArbFloat},), &z)
+    ccall((:arb_set_ui, libarb), Void, (Ptr{ArbFloat}, UInt), &z, x)
     finalizer(z, clearArbFloat)
     z
 end
 
 function ArbFloat(x::Int)
     z = ArbFloat{precision{ArbFloat}}(0,0,0,0,0,0)
-    ccall((:arb_init, :libarb), Void, (Ptr{ArbFloat},), &z)
-    ccall((:arb_set_si, :libarb), Void, (Ptr{ArbFloat}, Int), &z, x)
+    ccall((:arb_init, libarb), Void, (Ptr{ArbFloat},), &z)
+    ccall((:arb_set_si, libarb), Void, (Ptr{ArbFloat}, Int), &z, x)
     finalizer(z, clearArbFloat)
     z
 end
 
 function ArbFloat(x::Float64)
     z = ArbFloat{precision(ArbFloat)}(0,0,0,0,0,0)
-    ccall((:arb_init, :libarb), Void, (Ptr{ArbFloat},), &z)
-    ccall((:arb_set_d, :libarb), Void, (Ptr{ArbFloat}, Float64), &z, x)
+    ccall((:arb_init, libarb), Void, (Ptr{ArbFloat},), &z)
+    ccall((:arb_set_d, libarb), Void, (Ptr{ArbFloat}, Float64), &z, x)
     finalizer(z, clearArbFloat)
     z
 end
@@ -53,8 +53,8 @@ end
 function ArbFloat(x::BigFloat)
     p = precision(ArbFloat)
     z = ArbFloat{p}(0,0,0,0,0,0)
-    ccall((:arb_init, :libarb), Void, (Ptr{ArbFloat},), &z)
-    ccall((:arb_set_round_fmpz, :libarb), Void, (Ptr{ArbFloat}, BigFloat, Int), &z, x, p)
+    ccall((:arb_init, libarb), Void, (Ptr{ArbFloat},), &z)
+    ccall((:arb_set_round_fmpz, libarb), Void, (Ptr{ArbFloat}, BigFloat, Int), &z, x, p)
     finalizer(z, clearArbFloat)
     z
 end
@@ -64,8 +64,8 @@ function ArbFloat(x::String)
     p = precision(ArbFloat)
     b = bytestring(x)
     z = ArbFloat{p}(0,0,0,0,0,0)
-    ccall((:arb_init, :libarb), Void, (Ptr{ArbFloat},), &z)
-    ccall((:arb_set_str, :libarb), Void, (Ptr{ArbFloat}, Ptr{UInt8}, Int), &z, b, p)
+    ccall((:arb_init, libarb), Void, (Ptr{ArbFloat},), &z)
+    ccall((:arb_set_str, libarb), Void, (Ptr{ArbFloat}, Ptr{UInt8}, Int), &z, b, p)
     finalizer(z, clearArbFloat)
     z
 end
