fab90e4d5edabdf42c3dafe0178f1f3dbc581ab5
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-14 08:31:01 -0400

    Update arith.jl

--- src/math/arith.jl
+++ src/math/arith.jl
@@ -2,7 +2,6 @@
    -x, abs(x), inv(x),
    floor(x), ceil(x), sqrt(x), invsqrt(x)
    x+y, x-y, x*y, x/y, hypot(x,y)
-   x^y
 =#
 
 for (op,cfunc) in ((:-,:arb_neg), (:abs, :arb_abs))
@@ -31,7 +30,7 @@ for (op,cfunc) in ((:inv, :arb_inv), (:floor,:arb_floor), (:ceil, :arb_ceil),
 end
 
 for (op,cfunc) in ((:+,:arb_add), (:-, :arb_sub), (:*, :arb_mul), (:/, :arb_div), 
-    (:^, :arb_pow), (:hypot, arb_hypot))
+      (:hypot, arb_hypot))
   @eval begin
     function ($op){P}(x::ArbFloat{P}, y::ArbFloat{P})
       z = ArbFloat{P}(0,0,0,0,0,0)
