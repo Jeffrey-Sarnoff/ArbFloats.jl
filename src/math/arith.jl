2ef540813ae4c381018c6ce8e788b7289b4453c3
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-14 11:00:08 -0400

    Update arith.jl

--- src/math/arith.jl
+++ src/math/arith.jl
@@ -2,6 +2,7 @@
    -x, abs(x), inv(x),
    floor(x), ceil(x), sqrt(x), invsqrt(x)
    x+y, x-y, x*y, x/y, hypot(x,y)
+   muladd(a,b,c), fma(a,b,c)
 =#
 
 for (op,cfunc) in ((:-,:arb_neg), (:abs, :arb_abs))
@@ -10,7 +11,7 @@ for (op,cfunc) in ((:-,:arb_neg), (:abs, :arb_abs))
       z = ArbFloat{P}(0,0,0,0,0,0)
       ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}), &z, &x)
-      finalizer(z, clearArbFloat)      
+      finalizer(z)      
       z
     end
   end
@@ -23,7 +24,7 @@ for (op,cfunc) in ((:inv, :arb_inv), (:floor,:arb_floor), (:ceil, :arb_ceil),
       z = ArbFloat{P}(0,0,0,0,0,0)
       ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, P)
-      finalizer(z, clearArbFloat)      
+      finalizer(z)      
       z
     end
   end
@@ -35,7 +36,7 @@ for (op,cfunc) in ((:+,:arb_add), (:-, :arb_sub), (:*, :arb_mul), (:/, :arb_div)
       z = ArbFloat{P}(0,0,0,0,0,0)
       ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
-      finalizer(z, clearArbFloat)      
+      finalizer(z)      
       z
     end
   end
@@ -47,7 +48,7 @@ for (op,cfunc) in ((:addmul,:arb_addmul), (:submul, :arb_submul))
       z = ArbFloat{P}(0,0,0,0,0,0)
       ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &w, &x, &y, P)
-      finalizer(z, clearArbFloat)      
+      finalizer(z)      
       z
     end
   end
@@ -59,9 +60,13 @@ function (fma){P}(w::ArbFloat{P}, x::ArbFloat{P}, y::ArbFloat{P})
    P3=floor(Int,P*3.25)
    z = ArbFloat{P}(0,0,0,0,0,0)
    zz = ArbFloat{P3}(0,0,0,0,0,0)
+   ww = ArbFloat{P3}(w)
+   xx = ArbFloat{P3}(x)
+   yy = ArbFloat{P3}(y)
    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
-   ccall(@libarb(arb_addmul), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &zz, &w, &x, &y, P)
+   ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &zz)
+   ccall(@libarb(arb_addmul), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &zz, &ww, &xx, &yy, P)
    ccall(@libarb(arb_set_round), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &zz, &z, P)
-   finalizer(z, clearArbFloat)      
+   finalizer(z)      
    z
 end
