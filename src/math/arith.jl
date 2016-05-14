c5ccfda5cdc1358c0a87d1dbf19aaae3ae22afbc
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-14 06:00:47 -0400

    Update arith.jl

--- src/math/arith.jl
+++ src/math/arith.jl
@@ -3,8 +3,8 @@ for (op,cfunc) in ((:+,:arb_add), (:-, :arb_sub), (:*, :arb_mul), (:/, :arb_div)
     function ($op){P}(x::ArbFloat{P}, y::ArbFloat{P})
       z = ArbFloat{P}(0,0,0,0,0,0)
       ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+      finalizer(z, clearArbFloat)      
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
-      finalizer(z, clearArbFloat)
       z
     end
   end
