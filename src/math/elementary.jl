06f6f24140076d0f65ce6c55f3497f69c3ab8b6f
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-14 09:01:52 -0400

    Update elementary.jl

--- src/math/elementary.jl
+++ src/math/elementary.jl
@@ -50,3 +50,16 @@ for (op,cfunc) in ((:^,:arb_pow_ui), (:pow,:arb_pow_ui), (:root, :arb_root_ui))
     end
   end
 end
+
+
+for (op,cfunc) in ((:^,:arb_pow_ui), (:pow,:arb_pow))
+  @eval begin
+    function ($op){P}(x::ArbFloat{P}, y::ArbFloat{P})
+      z = ArbFloat{P}(0,0,0,0,0,0)
+      ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+      ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
+      finalizer(z, clearArbFloat)      
+      z
+    end
+  end
+end
