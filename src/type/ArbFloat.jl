0eda8f0f7b117d355173f94acd232434cd9d9706
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-14 02:18:10 -0400

    Update ArbFloat.jl

--- src/type/ArbFloat.jl
+++ src/type/ArbFloat.jl
@@ -7,7 +7,7 @@ type ArbFloat{Precision}  <: Real
   rad_man::UInt
 end
 
-setprecision(ArbFloat,120)
+Base.setprecision(ArbFloat,120)
 
 function clearArbFloat(x::ArbFloat)
      ccall((:arb_clear, :libarb), Void, (Ptr(ArbFloat),), &x)
@@ -17,10 +17,37 @@ function ArbFloat()
     z = ArbFloat{precision(ArbFloat)}(0,0,0,0,0,0)
     ccall((:arb_init, :libarb), Void, (Ptr{ArbFloat},), &z)
     finalizer(z, clearArbFloat)
+    z
 end
 
-function ArbFloat(x::Int)
+function ArbFloat(x::UInt)
     z = ArbFloat{precision(ArbFloat)}(0,0,0,0,0,0)
+    ccall((:arb_init, :libarb), Void, (Ptr{ArbFloat},), &z)
+    ccall((:arb_set_ui, :libarb), Void, (Ptr{ArbFloat}, UInt), &z, x)
+    finalizer(z, clearArbFloat)
+    z
+end
+
+function ArbFloat(x::Float64)
+    z = ArbFloat{precision(ArbFloat)}(0,0,0,0,0,0)
+    ccall((:arb_init, :libarb), Void, (Ptr{ArbFloat},), &z)
+    ccall((:arb_set_d, :libarb), Void, (Ptr{ArbFloat}, Float64), &z, x)
+    finalizer(z, clearArbFloat)
+    z
+end
+
+function ArbFloat(x::BigFloat)
+    z = ArbFloat{precision(ArbFloat)}(0,0,0,0,0,0)
+    ccall((:arb_init, :libarb), Void, (Ptr{ArbFloat},), &z)
+    ccall((:arb_set_fmpz, :libarb), Void, (Ptr{ArbFloat}, BigFloat), &z, x)
+    finalizer(z, clearArbFloat)
+    z
+end
+
+
+
+function ArbFloat(x::Int, sigbits::Int)
+    z = ArbFloat{sigbits}(0,0,0,0,0,0)
     ccall((:arb_init, :libarb), Void, (Ptr{ArbFloat},), &z)
     ccall((:arb_set_si, :libarb), Void, (Ptr{ArbFloat}, Int), &z, x)
     finalizer(z, clearArbFloat)
