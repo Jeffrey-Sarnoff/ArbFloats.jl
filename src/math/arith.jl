5251aefb35f6c7f8fc182c9e24da061815e4bce3
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-14 14:38:54 -0400

    Update arith.jl

--- src/math/arith.jl
+++ src/math/arith.jl
@@ -39,8 +39,8 @@ for (op,cfunc) in ((:+,:arb_add), (:-, :arb_sub), (:*, :arb_mul), (:/, :arb_div)
       finalizer(z)      
       z
     end
-    ($op){T<:AbstractFloat,P}(x::ArbFloat{P}, y::T) = ($op)(x, ArbFloat{P}(y))
-    ($op){T<:AbstractFloat,P}(x::T, y::ArbFloat{P}) = ($op)(ArbFloat{P}(x), y)
+    ($op){T<:AbstractFloat,P}(x::ArbFloat{P}, y::T) = ($op)(x, convert(ArbFloat{P}, y))
+    ($op){T<:AbstractFloat,P}(x::T, y::ArbFloat{P}) = ($op)(convert(ArbFloat{P}, x), y)
   end
 end
 
@@ -61,15 +61,15 @@ end
 (*){P}(x::Int, y::ArbFloat{P}) = (*)(y,x)
 (/){P}(x::Int, y::ArbFloat{P}) = (/)(ArbFloat{P}(x),y)
 
-(+){P}(x::ArbFloat{P}, y::Integer) = (+)(x, ArbFloat(y))
-(-){P}(x::ArbFloat{P}, y::Integer) = (-)(x, ArbFloat(y))
-(*){P}(x::ArbFloat{P}, y::Integer) = (*)(x, ArbFloat(y))
-(/){P}(x::ArbFloat{P}, y::Integer) = (/)(x, ArbFloat(y))
+(+){P}(x::ArbFloat{P}, y::Integer) = (+)(x, convert(ArbFloat{P}, y))
+(-){P}(x::ArbFloat{P}, y::Integer) = (-)(x, convert(ArbFloat{P}, y))
+(*){P}(x::ArbFloat{P}, y::Integer) = (*)(x, convert(ArbFloat{P}, y))
+(/){P}(x::ArbFloat{P}, y::Integer) = (/)(x, convert(ArbFloat{P}, y))
 
-(+){P}(x::Integer, y::ArbFloat{P}) = (+)(ArbFloat(x),y)
-(-){P}(x::Integer, y::ArbFloat{P}) = -((-)(y,ArbFloat(x)))
-(*){P}(x::Integer, y::ArbFloat{P}) = (*)(ArbFloat(x),y)
-(/){P}(x::Integer, y::ArbFloat{P}) = (/)(ArbFloat(x),y)
+(+){P}(x::Integer, y::ArbFloat{P}) = (+)(convert(ArbFloat{P}, x), y)
+(-){P}(x::Integer, y::ArbFloat{P}) = -((-)(y, convert(ArbFloat{P}, x)))
+(*){P}(x::Integer, y::ArbFloat{P}) = (*)(convert(ArbFloat{P},x), y)
+(/){P}(x::Integer, y::ArbFloat{P}) = (/)(convert(ArbFloat{P},x), y)
 
 for (op,cfunc) in ((:addmul,:arb_addmul), (:submul, :arb_submul))
   @eval begin
