eb4f322b3b2e35a7fa817bf5f81309cd48457b58
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-14 07:15:45 -0400

    Update arith.jl

--- src/math/arith.jl
+++ src/math/arith.jl
@@ -1,6 +1,7 @@
 #=
-   -x, abs(x), inv(x)
-   x+y, x-y, x*y, x/y
+   -x, abs(x), inv(x),
+   floor(x), ceil(x), sqrt(x),
+   x+y, x-y, x*y, x/y, hypot(x,y)
    x^y
 =#
 
@@ -16,15 +17,19 @@ for (op,cfunc) in ((:-,:arb_neg), (:abs, :arb_abs))
   end
 end
 
-function inv{P}(x::ArbFloat{P})
-    z = ArbFloat{P}(0,0,0,0,0,0)
-    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
-    ccall(@libarb(arb_inv), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}), Int), &z, &x, P)
-    finalizer(z, clearArbFloat)      
-    z
+for (op,cfunc) in ((:inv, :arb_inv), (:floor,:arb_floor), (:ceil, :arb_ceil), (:sqrt, :arb_sqrt))
+  @eval begin
+    function ($op){P}(x::ArbFloat{P})
+      z = ArbFloat{P}(0,0,0,0,0,0)
+      ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+      ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, P)
+      finalizer(z, clearArbFloat)      
+      z
+    end
+  end
 end
 
-for (op,cfunc) in ((:+,:arb_add), (:-, :arb_sub), (:*, :arb_mul), (:/, :arb_div), (:^, :arb_pow))
+for (op,cfunc) in ((:+,:arb_add), (:-, :arb_sub), (:*, :arb_mul), (:/, :arb_div), (:^, :arb_pow), (:hypot, arb_hypot))
   @eval begin
     function ($op){P}(x::ArbFloat{P}, y::ArbFloat{P})
       z = ArbFloat{P}(0,0,0,0,0,0)
