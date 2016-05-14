84de6e2803b7502824c198162ecec2a88dc9e7d2
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-14 08:45:07 -0400

    Update arith.jl

--- src/math/arith.jl
+++ src/math/arith.jl
@@ -41,3 +41,28 @@ for (op,cfunc) in ((:+,:arb_add), (:-, :arb_sub), (:*, :arb_mul), (:/, :arb_div)
     end
   end
 end
+
+for (op,cfunc) in ((:addmul,:arb_addmul), (:submul, :arb_submul))
+  @eval begin
+    function ($op){P}(w::ArbFloat{P}, x::ArbFloat{P}, y::ArbFloat{P})
+      z = ArbFloat{P}(0,0,0,0,0,0)
+      ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+      ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &w, &x, &y, P)
+      finalizer(z, clearArbFloat)      
+      z
+    end
+  end
+end
+
+muladd{P}(a::ArbFloat{P}, b::ArbFloat{P}, c::ArbFloat{P}) = addmul(c,a,b)
+
+function (fma){P}(w::ArbFloat{P}, x::ArbFloat{P}, y::ArbFloat{P})
+   P3=floor(Int,P*3.25)
+   z = ArbFloat{P}(0,0,0,0,0,0)
+   zz = ArbFloat{P3}(0,0,0,0,0,0)
+   ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+   ccall(@libarb(arb_addmul), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &zz, &w, &x, &y, P)
+   ccall(@libarb(arb_set_round), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &zz, &z, P)
+   finalizer(z, clearArbFloat)      
+   z
+end
