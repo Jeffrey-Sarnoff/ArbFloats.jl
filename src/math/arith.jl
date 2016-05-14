b8ea57031fcd7ae0faa62a8963efc6804587ff2a
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-14 05:44:17 -0400

    Update arith.jl

--- src/math/arith.jl
+++ src/math/arith.jl
@@ -1,7 +1,7 @@
 for (op,cfunc) in ((:+,:arb_add), (:-, :arb_sub), (:*, :arb_mul), (:/, :arb_div))
   @eval begin
     function ($op){P}(x::ArbFloat{P}, y::ArbFloat{P})
-      z = ArbFloat{p}(0,0,0,0,0,0)
+      z = ArbFloat{P}(0,0,0,0,0,0)
       ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
       finalizer(z, clearArbFloat)
