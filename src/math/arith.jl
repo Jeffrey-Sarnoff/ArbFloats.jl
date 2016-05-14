18bec7d762a08b98eeb03c2010213d8f162d9dca
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-14 08:27:41 -0400

    Update arith.jl

--- src/math/arith.jl
+++ src/math/arith.jl
@@ -1,6 +1,6 @@
 #=
    -x, abs(x), inv(x),
-   floor(x), ceil(x), sqrt(x),
+   floor(x), ceil(x), sqrt(x), invsqrt(x)
    x+y, x-y, x*y, x/y, hypot(x,y)
    x^y
 =#
@@ -17,7 +17,8 @@ for (op,cfunc) in ((:-,:arb_neg), (:abs, :arb_abs))
   end
 end
 
-for (op,cfunc) in ((:inv, :arb_inv), (:floor,:arb_floor), (:ceil, :arb_ceil), (:sqrt, :arb_sqrt))
+for (op,cfunc) in ((:inv, :arb_inv), (:floor,:arb_floor), (:ceil, :arb_ceil), 
+    (:sqrt, :arb_sqrt), (:invsqrt, :arb_rsqrt))
   @eval begin
     function ($op){P}(x::ArbFloat{P})
       z = ArbFloat{P}(0,0,0,0,0,0)
@@ -29,7 +30,8 @@ for (op,cfunc) in ((:inv, :arb_inv), (:floor,:arb_floor), (:ceil, :arb_ceil), (:
   end
 end
 
-for (op,cfunc) in ((:+,:arb_add), (:-, :arb_sub), (:*, :arb_mul), (:/, :arb_div), (:^, :arb_pow), (:hypot, arb_hypot))
+for (op,cfunc) in ((:+,:arb_add), (:-, :arb_sub), (:*, :arb_mul), (:/, :arb_div), 
+    (:^, :arb_pow), (:hypot, arb_hypot))
   @eval begin
     function ($op){P}(x::ArbFloat{P}, y::ArbFloat{P})
       z = ArbFloat{P}(0,0,0,0,0,0)
