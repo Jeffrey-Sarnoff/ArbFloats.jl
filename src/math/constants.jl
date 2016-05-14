88aeb3d2db6f0d81f9d7122eebb6a3118ddb76c4
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-14 10:51:53 -0400

    Update constants.jl

--- src/math/constants.jl
+++ src/math/constants.jl
@@ -7,37 +7,87 @@ function PI{P}(::Type{ArbFloat{P}})
     finalizer(z)
     z
 end
-function PI(::Type{ArbFloat})
-   p = precision(ArbFloat)
-   PI(ArbFloat{p})
+PI(::Type{ArbFloat}) = PI(ArbFloat{precision(ArbFloat)})
+
+function SQRTPI{P}(::Type{ArbFloat{P}})
+    z = ArbFloat{P}(0,0,0,0,0,0)
+    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+    ccall(@libarb(arb_const_sqrtpi), Void, (Ptr{ArbFloat}, Int), &z, P)
+    finalizer(z)
+    z
 end
+SQRTPI(::Type{ArbFloat}) = SQRTPI(ArbFloat{precision(ArbFloat)})
+
+function LOG2{P}(::Type{ArbFloat{P}})
+    z = ArbFloat{P}(0,0,0,0,0,0)
+    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+    ccall(@libarb(arb_const_log2), Void, (Ptr{ArbFloat}, Int), &z, P)
+    finalizer(z)
+    z
+end
+LOG2(::Type{ArbFloat}) = LOG2(ArbFloat{precision(ArbFloat)})
+
+function LOG10{P}(::Type{ArbFloat{P}})
+    z = ArbFloat{P}(0,0,0,0,0,0)
+    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+    ccall(@libarb(arb_const_log10), Void, (Ptr{ArbFloat}, Int), &z, P)
+    finalizer(z)
+    z
+end
+LOG10(::Type{ArbFloat}) = LOG10(ArbFloat{precision(ArbFloat)})
+
+function EXP1{P}(::Type{ArbFloat{P}})
+    z = ArbFloat{P}(0,0,0,0,0,0)
+    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+    ccall(@libarb(arb_const_e), Void, (Ptr{ArbFloat}, Int), &z, P)
+    finalizer(z)
+    z
+end
+EXP1(::Type{ArbFloat}) = EXP1(ArbFloat{precision(ArbFloat)})
 
 
-log2o10{P}(::Type{ArbFloat{P}}) = log(ArbFloat{P}(2))/log(ArbFloat{P}(10))
-log10o2{P}(::Type{ArbFloat{P}}) = log(ArbFloat{P}(10))/log(ArbFloat{P}(2))
+function EULER{P}(::Type{ArbFloat{P}})
+    z = ArbFloat{P}(0,0,0,0,0,0)
+    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+    ccall(@libarb(arb_const_euler), Void, (Ptr{ArbFloat}, Int), &z, P)
+    finalizer(z)
+    z
+end
+EULER(::Type{ArbFloat}) = EULER(ArbFloat{precision(ArbFloat)})
 
-exp1{P}(::Type{ArbFloat{P}})    = exp(ArbFloat{P}(1))
+function CATALAN{P}(::Type{ArbFloat{P}})
+    z = ArbFloat{P}(0,0,0,0,0,0)
+    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+    ccall(@libarb(arb_const_catalan), Void, (Ptr{ArbFloat}, Int), &z, P)
+    finalizer(z)
+    z
+end
+CATALAN(::Type{ArbFloat}) = CATALAN(ArbFloat{precision(ArbFloat)})
 
-fourpi{P}(::Type{ArbFloat{P}})  = ArbFloat{P}(16)*atan(ArbFloat{P}(1))
-twopi{P}(::Type{ArbFloat{P}})   = ArbFloat{P}(8)*atan(ArbFloat{P}(1))
-onepi{P}(::Type{ArbFloat{P}})   = ArbFloat{P}(4)*atan(ArbFloat{P}(1))
-halfpi{P}(::Type{ArbFloat{P}})  = ArbFloat{P}(2)*atan(ArbFloat{P}(1))
-qrtrpi{P}(::Type{ArbFloat{P}})  = atan(ArbFloat{P}(1))
+function KINCHIN{P}(::Type{ArbFloat{P}})
+    z = ArbFloat{P}(0,0,0,0,0,0)
+    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+    ccall(@libarb(arb_const_kinchin), Void, (Ptr{ArbFloat}, Int), &z, P)
+    finalizer(z)
+    z
+end
+KHINCHIN(::Type{ArbFloat}) = KINCHIN(ArbFloat{precision(ArbFloat)})
 
+function GLAISHER{P}(::Type{ArbFloat{P}})
+    z = ArbFloat{P}(0,0,0,0,0,0)
+    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+    ccall(@libarb(arb_const_apery), Void, (Ptr{ArbFloat}, Int), &z, P)
+    finalizer(z)
+    z
+end
+GLAISHER(::Type{ArbFloat}) = GLAISHER(ArbFloat{precision(ArbFloat)})
 
-golden{P}(::Type{ArbFloat{P}})  = (1+sqrt(ArbFloat{P}(4)))/ArbFloat{P}(2)
+function APERY{P}(::Type{ArbFloat{P}})
+    z = ArbFloat{P}(0,0,0,0,0,0)
+    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat},), &z)
+    ccall(@libarb(arb_const_apery), Void, (Ptr{ArbFloat}, Int), &z, P)
+    finalizer(z)
+    z
+end
+APERY(::Type{ArbFloat}) = APERY(ArbFloat{precision(ArbFloat)})
 
-
-
-log2o10(::Type{ArbFloat}) = log2o10(ArbFloat{precision(ArbFloat)})
-log10o2(::Type{ArbFloat}) = log10o2(ArbFloat{precision(ArbFloat)})
-
-exp1(::Type{ArbFloat}) = exp1(ArbFloat{precision(ArbFloat)})
-
-fourpi(::Type{ArbFloat}) = fourpi(ArbFloat{precision(ArbFloat)})
-twopi(::Type{ArbFloat}) = twopi(ArbFloat{precision(ArbFloat)})
-onepi(::Type{ArbFloat}) = onepi(ArbFloat{precision(ArbFloat)})
-halfpi(::Type{ArbFloat}) = halfpi(ArbFloat{precision(ArbFloat)})
-qrtrpi(::Type{ArbFloat}) = qrtrpi(ArbFloat{precision(ArbFloat)})
-
-golden(::Type{ArbFloat}) = golden(ArbFloat{precision(ArbFloat)})
