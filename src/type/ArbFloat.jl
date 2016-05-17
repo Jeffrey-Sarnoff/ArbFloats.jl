ff54631aa28d10c4781128e4287e37b64f45d37a
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-17 19:38:35 -0400

    Update ArbFloat.jl

--- src/type/ArbFloat.jl
+++ src/type/ArbFloat.jl
@@ -142,16 +142,7 @@ function show{P}(io::IO, x::ArbFloat{P})
     print(io, s)
 end
 
-ArbFloatHalf = Dict{Int,ArbFloat}(  120 => ArbFloat{120}(0.5),
-                                    122 => ArbFloat{123}(0.5),
-                                    125 => ArbFloat{125}(0.5),
-                                    128 => ArbFloat{128}(0.5),
-                                    160 => ArbFloat{160}(0.5),
-                                    185 => ArbFloat{185}(0.5),
-                                    192 => ArbFloat{192}(0.5),
-                                    250 => ArbFloat{250}(0.5),
-                                    );
-                                    
+
 # fractionalPart(x), wholePart(x) = modf(x)
 #
 function stringInformed{P}(x::ArbFloat{P})
