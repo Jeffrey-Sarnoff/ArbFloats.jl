eb4e3256d788415327b91c9799e5f83f80ac6ef2
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-18 02:54:29 -0400

    Update primitive.jl

--- src/type/primitive.jl
+++ src/type/primitive.jl
@@ -1,2 +1,31 @@
+
+function copy{P}(x::ArbFloat{P})
+    z = initializer(ArbFloat{P})
+    ccall(@libarb(arb_set), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}), &z, &x)
+    z
+end
+
+function deepcopy{P}(x::ArbFloat{P})
+    z = initializer(ArbFloat{P})
+    ccall(@libarb(arb_set), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}), &z, &x)
+    z
+end
+
+
+function decompose{P}(x::ArbFloat{P})
+    # decompose x as num * 2^pow / den
+    # num, pow, den = decompose(x)
+    bf = convert(BigFloat, x)
+    decompose(bf)
+end
+
+function round{P}(x::ArbFloat{P}; sigbits::Int=P)
+   z = initializer(ArbFloat{P})
+   ccall(@libarb(arb_set_round), Void,  (Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, sigbits)
+   z
+end
+round{P}(x::ArbFloat{P}, prec::Int) = round(x,sigbits=prec)
+
+
 eps{P}(::Type{ArbFloat{P}}) = ldexp(1.0,-P) # for intertype workings
 eps{P}(x::ArbFloat{P}) = ldexp(1.0,-P)*x    # for intratype workings
