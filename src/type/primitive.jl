d8041044d1b78961c256d5aaa9c8e3a7a009b971
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-05-23 01:41:37 -0400

    Update primitive.jl

--- src/type/primitive.jl
+++ src/type/primitive.jl
@@ -24,6 +24,16 @@ function deepcopyradius{P}(target::ArbFloat{P}, source::ArbFloat{P})
     target
 end
 
+function copymidpoint{P}(target::ArbFloat{P}, source::ArbFloat{P})
+    z = deepcopy(target)
+    z.mid_exp = source.mid_exp
+    z.mid_size = source.mid_size
+    z.mid_d1 = source.mid_d1
+    z.mid_d2 = source.mid_d2
+    z
+end
+
+
 function decompose{P}(x::ArbFloat{P})
     # decompose x as num * 2^pow / den
     # num, pow, den = decompose(x)
