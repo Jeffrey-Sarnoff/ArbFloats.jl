15906498947f8868611d3c0ed653644b7d2c7d06
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-23 04:29:53 -0400

    Update primitive.jl

--- src/type/primitive.jl
+++ src/type/primitive.jl
@@ -45,68 +45,6 @@ function decompose{P}(x::ArbFloat{P})
     n,p,d
 end
 
-function round{P}(x::ArbFloat{P}, sig::Int=P, base::Int=10)
-    sig=abs(sig); base=abs(base)
-    sigbits = ceil(Int, (sig * log(base)/log(2.0)))
-    sigbits = min(P,sigbits)
-    z = ArbFloat{P}(0,0,0,0,0,0)
-    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat{P}},), &z)
-    finalizer(z, clearArbFloat)
-    ccall(@libarb(arb_set_round), Void,  (Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, sigbits)
-    z
-end
-
-function ceil{P}(x::ArbFloat{P}, sig::Int=P, base::Int=10)
-    sig=abs(sig); base=abs(base)
-    sigbits = ceil(Int, (sig * log(base)/log(2.0)))
-    sigbits = min(P,sigbits)
-    z = ArbFloat{P}(0,0,0,0,0,0)
-    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat{P}},), &z)
-    finalizer(z, clearArbFloat)
-    ccall(@libarb(arb_ceil), Void,  (Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, sigbits)
-    z
-end
-
-function floor{P}(x::ArbFloat{P}, sig::Int=P, base::Int=10)
-    sig=abs(sig); base=abs(base)
-    sigbits = ceil(Int, (sig * log(base)/log(2.0)))
-    sigbits = min(P,sigbits)
-    z = ArbFloat{P}(0,0,0,0,0,0)
-    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat{P}},), &z)
-    finalizer(z, clearArbFloat)
-    ccall(@libarb(arb_floor), Void,  (Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, sigbits)
-    z
-end
-
-function trunc{P}(x::ArbFloat{P}, sig::Int=P, base::Int=10)
-    sig=abs(sig); base=abs(base)
-    sigbits = ceil(Int, (sig * log(base)/log(2.0)))
-    sigbits = min(P,sigbits)
-    z = ArbFloat{P}(0,0,0,0,0,0)
-    ccall(@libarb(arb_init), Void, (Ptr{ArbFloat{P}},), &z)
-    finalizer(z, clearArbFloat)
-    y = abs(x)
-    ccall(@libarb(arb_floor), Void,  (Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &y, sigbits)
-    signbit(x) ? -z : z
-end
-
-function round{T,P}(::Type{T}, x::ArbFloat{P}, sig::Int=P, base::Int=10)
-    z = round(x, sig, base)
-    convert(T, z)
-end
-function ceil{T,P}(::Type{T}, x::ArbFloat{P}, sig::Int=P, base::Int=10)
-    z = ceil(x, sig, base)
-    convert(T, z)
-end
-function floor{T,P}(::Type{T}, x::ArbFloat{P}, sig::Int=P, base::Int=10)
-    z = floor(x, sig, base)
-    convert(T, z)
-end
-function trunc{T,P}(::Type{T}, x::ArbFloat{P}, sig::Int=P, base::Int=10)
-    z = trunc(x, sig, base)
-    convert(T, z)
-end
-
 
 eps{P}(::Type{ArbFloat{P}}) = ldexp(1.0,-P) # for intertype workings
 eps{P}(x::ArbFloat{P}) = ldexp(1.0,-P)*x    # for intratype workings
