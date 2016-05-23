c89591722ad9c31062169cb1b165dda0dccaa56b
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-23 00:55:27 -0400

    Update convert.jl

--- src/type/convert.jl
+++ src/type/convert.jl
@@ -98,7 +98,7 @@ end
 convert{P}(::Type{Float32}, x::ArbFloat{P}) = convert(Float32,convert(Float64,x))
 
 function convert{P}(::Type{Int}, x::ArbFloat{P})
-    s = num2str(x,22)
+    s = num2str(x, trunc(Int, P*0.30103)) # P * log(2)/log(10)
     trunc(Int,parse(Float64,s))
     #ccall(@libarb(arb_set_si), Void, (Ptr{ArbFloat{P}}, Int), &x, z)
     #z
