7bd66493cb22c62afdc398c868ad8e1921fe6595
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-23 03:04:00 -0400

    Update special.jl

--- src/math/special.jl
+++ src/math/special.jl
@@ -1,3 +1,14 @@
+for (op,cfunc) in ((:factorial,:arb_fac_ui), (:doublefactorial,:arb_doublefac_ui), (:risingfactorial, :arb_rising_ui))
+  @eval begin
+    function ($op){P}(x::ArbFloat{P}, y::Int)
+      yy = UInt(y)
+      z = initializer(ArbFloat{P})
+      ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, UInt, Int), &z, &x, &yy, P)
+      z
+    end
+  end
+end
+
 for (op,cfunc) in ((:agm, :arb_agm), (:polylog, :arb_polylog))
   @eval beg
     function ($op){P}(x::ArbFloat{P}, y::ArbFloat{P}, prec::Int=P)
