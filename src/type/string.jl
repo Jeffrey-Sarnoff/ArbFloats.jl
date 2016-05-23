799f24350a44b827fbb3c9e6914c57005059f71e
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-23 06:02:30 -0400

    Update string.jl

--- src/type/string.jl
+++ src/type/string.jl
@@ -48,7 +48,7 @@ end
 
 function stringTrimmed{P}(x::ArbFloat{P}, ndigitsremoved::Int)
    n = max(4,floor(Int, P*0.3010299956639811952137))-ndigitsremoved
-   cstr = ccall(@libarb(arb_get_str), Ptr{UInt8}, (Ptr{ArbFloat}, Int, UInt), &x, n, UInt(4)) # round nearest
+   cstr = ccall(@libarb(arb_get_str), Ptr{UInt8}, (Ptr{ArbFloat}, Int, UInt), &x, n, UInt(2)) # no radius, round nearest
    s = String(cstr)
    ccall(@libflint(flint_free), Void, (Ptr{UInt8},), cstr)
    s
