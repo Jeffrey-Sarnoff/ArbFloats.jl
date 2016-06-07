3c077c46a24fd066d7aa02de1cc39a0369502c99
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-06-07 01:09:33 -0400

    Update io.jl

--- src/type/io.jl
+++ src/type/io.jl
@@ -8,45 +8,55 @@ function showall{P}(io::IO, x::ArbFloat{P})
     print(io, s)
 end
 
-#=
-function showall{P,N}(io::IO, x::NTuple{N,ArbFloat{P}})
-    for z in x
-      s = string(midpoint(z)," ± ", convert(Float64,radius(z)))
-      print(io, s);print(io,"\n")
-    end
+
+function stringall{P}(x::ArbFloat{P})
+    string(midpoint(x)," ± ", convert(Float64,radius(x)))
 end
-=#
 
 function showall{P,N}(io::IO, x::NTuple{N,ArbFloat{P}})
     if N==0
        print(io,"()")
        return nothing
     elseif N==1
-       print(io,string("(",midpoint(x[1])," ± ", convert(Float64,radius(x[1])),")"))
+       print(io,string("( ",stringall(x[1]),", )"))
+       return nothing
     end
     
     ss = Vector{String}(N)
     for i in 1:N
-      z = x[i]
-      ss[i] = string(midpoint(z)," ± ", convert(Float64,radius(z)))
+      ss[i] = stringall(x[i])
     end
     
     println(io,string("( ", ss[1], ","));
     for s in ss[2:end-1]
       println(io, string("  ", s, ","))
     end  
-    println(io,string("  ", ss[end], ")"))
+    println(io,string("  ", ss[end], " )"))
 end
 
 function showall{P}(io::IO, x::Vector{ArbFloat{P}})
     n = length(x)
+
+    if n==0
+       print(io,"[]")
+       return nothing
+    elseif n==1
+       print(io,string("[ ",stringall(x[1])," ]"))
+       return nothing
+    end
+    
     ss = Vector{String}(n)
     for i in 1:n
-      z = x[i]
-      ss[i] = string(midpoint(z)," ± ", convert(Float64,radius(z)))
+      ss[i] = stringall(x[i])
     end
-    print(io, ss)
+    
+    println(io,string("[ ", ss[1], ","));
+    for s in ss[2:end-1]
+      println(io, string("  ", s, ","))
+    end  
+    println(io,string("  ", ss[end], " ]"))
 end
+
 
 function showcompact{P}(io::IO, x::ArbFloat{P})
     showcompact(io, Float64(midpoint(x)))
