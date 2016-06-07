333f00d9f15bc8941abab91fa9a878d55e080c75
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-06-07 00:58:14 -0400

    Update io.jl

--- src/type/io.jl
+++ src/type/io.jl
@@ -23,7 +23,11 @@ function showall{P,N}(io::IO, x::NTuple{N,ArbFloat{P}})
       z = x[i]
       ss[i] = string(midpoint(z)," ± ", convert(Float64,radius(z)))
     end
-    print(io, (ss...))
+    print(io,"( ");
+    for s in ss[1:end-1]
+      println(io, string(s,",\n"))
+    end  
+    println(io,string(ss[end],")"))
 end
 
 function showall{P}(io::IO, x::Vector{ArbFloat{P}})
