d32994685a5fc53de000279ae8dc3a9270cf1fe1
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-06-15 22:17:42 -0400

    Update string.jl

--- src/type/string.jl
+++ src/type/string.jl
@@ -89,153 +89,25 @@ function smartarbstring{P}(x::ArbFloat{P})
      ubs
 end
 
-
-#=
-function stringRoundNearest{P}(x::ArbFloat{P})
-   cstr = ccall(@libarb(arb_get_str), Ptr{UInt8}, (Ptr{ArbFloat}, Int, UInt), &x, P, UInt(2)) # round nearest
-   s = String(cstr)
-   ccall(@libflint(flint_free), Void, (Ptr{UInt8},), cstr)
-   s
-end
-function stringRoundFromZeroWithRadius{P}(x::ArbFloat{P})
-   cstr = ccall(@libarb(arb_get_str), Ptr{UInt8}, (Ptr{ArbFloat}, Int, UInt), &x, P, UInt(1)) # round up
-   s = String(cstr)
-   ccall(@libflint(flint_free), Void, (Ptr{UInt8},), cstr)
-   s
-end
-function stringRoundToZeroWithRadius{P}(x::ArbFloat{P})
-   cstr = ccall(@libarb(arb_get_str), Ptr{UInt8}, (Ptr{ArbFloat}, Int, UInt), &x, P, UInt(0)) # round down
-   s = String(cstr)
-   ccall(@libflint(flint_free), Void, (Ptr{UInt8},), cstr)
-   s
-end
-function stringRoundFromZero{P}(x::ArbFloat{P})
-   cstr = ccall(@libarb(arb_get_str), Ptr{UInt8}, (Ptr{ArbFloat}, Int, UInt), &x, P, UInt(3)) # round ceil
-   s = String(cstr)
-   ccall(@libflint(flint_free), Void, (Ptr{UInt8},), cstr)
-   s
-end
-=#
-
-
-#=
-function smartarbstring{P}(x::ArbFloat{P})
-    if midpoint(x)==0 && notexact(x)
-       s = string(radius(x))
-       if contains(s, "e")
-          s1,s2 = split(s,"e")
-          if length(s2)>0
-             return string("±", rstrip(s1,'0'),"e",s2)
-          else
-             return string("±", rstrip(s1,'0'))
-          end   
-       else
-          return string("±", rstrip(s,'0'))
-       end
-    end
-
-    
-    ub = upperbound(x)
-    lb = lowerbound(x)
-    
-    if (ub==lb)
-       if ub.rad_man==0
-          if ub.mid_d2==0
-              return string(Float64(ub))
-          else
-              return string(ub)
-          end
-       else
-           return string(ub)
-       end
-    end
-    
-    ubstr = stringTrimmed(ub,0)
-    lbstr = stringTrimmed(lb,0)
-    ubstrlen = length(ubstr)
-    lbstrlen = length(lbstr)
-    ubdelta = lbdelta = 0
-    if ubstrlen > lbstrlen
-       ubdelta = lbstrlen-ubstrlen
-    else
-       lbdelta = lbstrlen-ubstrlen
-    end
-    
-    for i in 0:(ubstrlen+ubdelta+lbdelta-4)
-        ubstr = stringTrimmed(ub,i-ubdelta)
-        lbstr = stringTrimmed(lb,i-lbdelta)
-        if ubstr==lbstr
-           break
-        end
-    end
-    
-    ubstr
+function smartvalue{P}(x::ArbFloat{P})
+    s = smartarbstring(x)
+    ArbFloat{P}(s)
 end    
-=#
-
+    
 function smartstring{P}(x::ArbFloat{P})
-    if midpoint(x)==0 && notexact(x)
-       s = string(radius(x))
-       if contains(s, "e")
-          s1,s2 = split(s,"e")
-          if length(s2)>0
-             return string("±", rstrip(s1,'0'),"e",s2)
-          else
-             return string("±", rstrip(s1,'0'))
-          end   
-       else
-          return string("±", rstrip(s,'0'))
-       end
-    end
-
-    ub = upperbound(x)
-    lb = lowerbound(x)
-    
-    if (ub==lb)
-       if ub.rad_man==0
-          if ub.mid_d2==0
-              return string(Float64(ub))
-          else
-              return string(ub)
-          end
-       else
-           return string(ub,"~")
-       end
-    end
-    
-    ubstr = stringTrimmed(ub,0)
-    lbstr = stringTrimmed(lb,0)
-    ubstrlen = length(ubstr)
-    lbstrlen = length(lbstr)
-    ubdelta = lbdelta = 0
-    if ubstrlen > lbstrlen
-       ubdelta = lbstrlen-ubstrlen
-    else
-       lbdelta = lbstrlen-ubstrlen
-    end
-    
-    for i in 0:(ubstrlen+ubdelta+lbdelta-4)
-        ubstr = stringTrimmed(ub,i-ubdelta)
-        lbstr = stringTrimmed(lb,i-lbdelta)
-        if ubstr==lbstr
-           break
+    s = smartarbstring(x)
+    a = ArbFloat{P}(s)
+    postfix = 
+        if (upperbound(x) < a)   
+            "-"
+        elseif (lowerbound(x) > a)
+            "+"
+        else
+            "~"
         end
-    end
-    
-    bfprec=precision(BigFloat)
-    setprecision(BigFloat,P+64)
-    sval = round(parse(BigFloat,ubstr),P,2)
-    mval = round(BigFloat(midpoint(x)),P,2)
-    if sval > mval
-       ubstr = string(ubstr,"₋")
-    elseif sval < mval
-       ubstr = string(ubstr,"₊")
-    else
-       ubstr = string(ubstr,"~")
-    end
-    setprecision(BigFloat,bfprec)
-    ubstr
-end    
+    string(s,postfix)
+end
+
 
 function stringAll{P}(x::ArbFloat{P})
     string(midpoint(x)," ± ", string(radius(x),17))
